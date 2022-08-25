724c3d601cdd46ce251563b66729808e284f290c
zzennin <53431568+chaelin0722@users.noreply.github.com>
2022-08-25 12:39:51 -0400

    Update adaboost.py
    option 없이 
    clf = AdaBoostClassifier()
    clf.fit(X, Y)
    이렇게만 쓰는게 성능이 더 좋음

--- adaboost.py
+++ adaboost.py
@@ -50,33 +50,47 @@ X_train = np.concatenate((X_train_a, X_train_n), axis=0)
 Y_n=np.zeros(100)
 Y_a=np.ones(100)
 Y = np.concatenate((Y_a, Y_n), axis=0)
-
 '''
 # x1 = rr, x2 = pr, x3 = st
 ## rr, st
+
 X_train = [[0.9,  0.1], [0.8, 0.09], [1.2,0.11], [1.0, 0.12],
            [0.6, 0.11], [1.8, 0.18], [0.35, 0.05], [1.7, 0.17],
            [0.4, 0.06], [1.5, 0.04]]
-## pr, st
+
 '''
+## pr, st
+
 X_train = [[0.16, 0.1], [0.14, 0.09], [0.18,0.11], [0.13, 0.12],
            [0.2, 0.11], [0.3, 0.18], [0.08, 0.05], [0.09, 0.17],
            [0.25, 0.06], [0.28, 0.04]]
 '''
 '''
+
 ## rr, pr
 X_train = [[0.9, 0.1], [0.8, 0.14], [1.2,0.18], [1.0, 0.13],
            [0.6, 0.2], [1.8, 0.3], [0.35, 0.08], [1.7, 0.09],
            [0.4, 0.25], [1.5, 0.28]]
-           '''
+'''
 Y = [0,0,0,0,0,1,1,1,1,1]
+'''
+X_train = [[0.9, 0.16, 0.1], [0.8, 0.14, 0.09], [1.2,0.18,0.11], [1.0, 0.13, 0.12],
+           [0.6, 0.2, 0.11], [1.8, 0.3, 0.18], [0.35, 0.08, 0.05], [1.7, 0.09, 0.17],
+           [0.4, 0.25, 0.06], [1.5, 0.28, 0.04]]
+Y = [0,0,0,0,0,1,1,1,1,1]
+'''
 
 data = pd.DataFrame({'RR':[0.9, 0.8, 1.2, 1.0, 0.6, 1.8, 0.35, 1.7, 0.4, 1.5] ,
                      'PR':[0.1, 0.14, 0.18, 0.13, 0.2, 0.3, 0.08, 0.09, 0.25, 0.28],
                      'class':['normal','normal','normal','normal','normal', 'abnormal', 'abnormal', 'abnormal', 'abnormal', 'abnormal']})
 
-X_test = [[1.05, 0.165], [0.5, 0.3], [0.85, 0.17], [0.45, 0.085], [0.7, 0.28]]
 
+# X_test = [[1.05, 0.165, 0.095], [0.5, 0.3, 0.07], [0.85, 0.17, 0.14], [0.45, 0.085, 0.096], [0.7, 0.28, 0.085]]
+
+X_test = [[1.05, 0.095], [0.5, 0.07], [0.85, 0.14], [0.45, 0.096], [0.7, 0.085]]
+
+X_test_n =[[1.05, 0.095]]
+X_test_ab = [[0.5, 0.07], [0.85, 0.14], [0.45, 0.096], [0.7, 0.085]]
 Y_test = [0,1,1,1,1]
 
 #X = X_train
@@ -84,6 +98,8 @@ X = np.array(X_train)
 Y = np.array(Y)
 X_test = np.array(X_test)
 Y_test = np.array(Y_test)
+X_test_n = np.array(X_test_n)
+X_test_ab = np.array(X_test_ab)
 
 
 def plot_decision_boundary(classifier, X, y, N=10, scatter_weights=np.ones(len(Y)), ax=None):
@@ -111,26 +127,43 @@ def plot_decision_boundary(classifier, X, y, N=10, scatter_weights=np.ones(len(Y
         ax = plt.gca()
     ax.contourf(xx, yy, Z, 2, cmap='RdBu', alpha=.5)
     ax.contour(xx, yy, Z, 2, cmap='RdBu')
-    ax.scatter(X[:, 0], X[:, 1], c=y, cmap=cm_bright, s=scatter_weights * 40)
+    #ax.scatter(X[:, 0], X[:, 1], c=y, cmap=cm_bright, s=scatter_weights * 40)
+    ##
+    ax.scatter(X_test_n[:, 0], X_test_n[:, 1], c='red')
+    ax.scatter(X_test_ab[:, 0], X_test_ab[:, 1], c='blue')
     ax.set_xlabel('$RR$')
     ax.set_ylabel('$ST$')
 
 
 
+#for i in range(200):
+#n_estimators = i + 1
+
 base_estimator = DecisionTreeClassifier(max_depth=1, random_state=0)
 adaboost = AdaBoostClassifier(base_estimator=base_estimator,
-                              n_estimators=200, algorithm="SAMME",
+                              n_estimators=50, algorithm="SAMME",
                               random_state=0)
+
 adaboost.fit(X,Y)
 preds = adaboost.predict(X_test)
+score = adaboost.score(X_test, Y_test)
+
+#print("predict : ",preds)
+#print("scores : ",score)
+
+
+
 
 clf = AdaBoostClassifier()
 clf.fit(X, Y)
 
-plot_decision_boundary(adaboost, X, Y, N = 50)#, weights)
-plt.show()
 
 score = clf.score(X_test,Y_test)
-#preds = clf.predict(X_test)
+preds = clf.predict(X_test)
 
-print("results : ",preds)
+print("score", score)
+print("preds", preds)
+
+
+plot_decision_boundary(clf, X, Y, N = 50)#, weights)
+plt.show()
